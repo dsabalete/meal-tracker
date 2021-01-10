1e5c906d53f242d1f92a50e851728fece0c4bb91
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-01-10 22:24:04 +0100

    feat: add firebase module

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,22 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/firebase'],
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyAkM5o5yrUuTj3NQY3_63_TksPDHpYYMVE',
+      authDomain: 'meal-tracker-9b7e5.firebaseapp.com',
+      databaseURL: 'https://meal-tracker-9b7e5.firebaseio.com',
+      projectId: 'meal-tracker-9b7e5',
+      storageBucket: 'meal-tracker-9b7e5.appspot.com',
+      messagingSenderId: '1067596219075',
+      appId: '1:1067596219075:web:714a13bc7227c4f33d5876',
+      measurementId: 'G-MM5P1TRKHQ',
+    },
+    services: {
+      auth: true, // Just as example. Can be any other service.
+    },
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
