8d63503f7e0256a04bdf590a83b7e4174b640a39
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-01-20 00:07:41 +0100

    feat: create header component

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="bg-gray-200 text-blue-400 dark:bg-gray-900 p-2">
+    <MealTrackerIcon />
+    <h1>Meal Tracker</h1>
+  </header>
+</template>
+
+<script>
+import MealTrackerIcon from '@/assets/icons/restaurant.svg?inline'
+
+export default {
+  name: 'Header',
+  components: {
+    MealTrackerIcon,
+  },
+}
+</script>
