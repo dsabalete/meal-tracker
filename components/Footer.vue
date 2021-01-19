281ba4d93c12112afc36cb5aebe4171b30a59cd0
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-01-20 00:07:55 +0100

    feat: create footer component

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer
+    class="p-2 text-blue-700 dark:text-blue-400 dark:bg-gray-900 text-center"
+  >
+    David Sabalete &copy; 2021
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
