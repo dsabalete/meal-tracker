b5aab98366313a70c78cb901fc14acd2f688edec
David Sabalete <dsabalete@gmail.com>
2021-02-14 23:36:26 +0100

    feat: create ProfileIcon story

---
+++ stories/ProfileIcon.stories.js
@@ -0,0 +1,8 @@
+import ProfileIcon from '~/components/ProfileIcon.vue'
+
+export default {
+  title: 'ProfileIcon',
+  component: ProfileIcon,
+}
+
+export const defaultProfileIcon = () => '<ProfileIcon />'
