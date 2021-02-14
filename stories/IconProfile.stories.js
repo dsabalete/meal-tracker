df69af5d02e973ca5457480bc87a2cd1927286f0
David Sabalete <dsabalete@gmail.com>
2021-02-14 23:37:30 +0100

    refactor: remove obsolete story

--- stories/IconProfile.stories.js
+++
@@ -1,8 +0,0 @@
-import IconProfile from '~/components/IconProfile.vue'
-
-export default {
-  title: 'IconProfile',
-  component: IconProfile,
-}
-
-export const defaultIconProfile = () => '<IconProfile />'
