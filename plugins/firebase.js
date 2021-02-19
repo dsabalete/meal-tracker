493e29d5b67513ae2a48cf878b2b27261026d50b
David Sabalete <dsabalete@gmail.com>
2021-02-20 00:31:49 +0100

    fix: remove firebase plugin

--- plugins/firebase.js
+++
@@ -1,7 +0,0 @@
-export default (ctx, inject) => {
-  if (process.client) {
-    ctx.$fireModule.auth().onAuthStateChanged((user) => {
-      ctx.store.dispatch('Login/fetchUser', user)
-    })
-  }
-}
